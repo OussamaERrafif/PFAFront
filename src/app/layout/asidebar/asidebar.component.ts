8972fa6a8a4e27adc791d796ee2d56af2237fe62
OussamaERrafif <oussamaerra2002@gmail.com>
2024-04-28 20:10:04 +0100

    oussama

--- src/app/layout/asidebar/asidebar.component.ts
+++ src/app/layout/asidebar/asidebar.component.ts
@@ -1,15 +1,79 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-asidebar',
   standalone: true,
+  imports: [NgFor],
   templateUrl: './asidebar.component.html',
   styleUrls: ['./asidebar.component.css']
 })
 export class AsidebarComponent {
   isSubMenuOpen: boolean = false;
 
+  sidebarItems = {
+    "Home": [
+      {
+        label: "Dashboard",
+        href: "/dashboard",
+        icon: "svg path or icon class",
+        count: 5
+      },
+      {
+        label: "Profile",
+        submenu: [
+          { label: "Account", href: "/acc" },
+          { label: "Security", href: "/security" },
+          { label: "Billings", href: "/bills" }
+        ]
+      },
+      {
+        label: "Projects",
+        href: "/project",
+        count: 12
+      },
+      {
+        label: "Calendar",
+        href: "/calendar",
+        count: "20+"
+      },
+      {
+        label: "Documents",
+        href: "/document"
+      },
+      {
+        label: "Reports",
+        href: "/inventory"
+      }
+    ],
+    "Authentication": [
+      {
+        label: "Sign In",
+        href: "/signup",
+        icon: "S"
+      },
+      {
+        label: "Forgot Password",
+        href: "/forgotpassword",
+        icon: "F"
+      },
+      {
+        label: "W",
+        href: "#"
+      }
+    ],
+    "Settings": [
+      {
+        label: "Settings",
+        href: "/settings",
+        icon: "svg path or icon class"
+      }
+    ]
+  };
+  
+  
   toggleSubMenu() {
     this.isSubMenuOpen = !this.isSubMenuOpen;
   }
 }
+
