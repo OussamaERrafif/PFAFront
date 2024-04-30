6fd0cadc3b504373e9349145e26cf1b6c633b1dc
OussamaERrafif <oussamaerra2002@gmail.com>
2024-04-30 14:56:19 +0100

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './layout/sidebar.component';
-import { HeaderComponent } from './layout/header.component';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { AsidebarComponent } from './layout/asidebar/asidebar.component';
-import { HeroComponent } from './pages/apps/hero/hero.component';
-import { NgIf } from '@angular/common';
-import { HeroService } from './pages/apps/hero/heroservice.compnent';
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { AsidebarComponent } from "./layout/asidebar/asidebar.component";
+import { HomepageComponent } from "./pages/apps/homepage/homepage.component";
+
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  template: `
-    <ng-container *ngIf="condition; else elseBlock">
-      <app-asidebar></app-asidebar>
-      <app-navbar></app-navbar>
-      <router-outlet></router-outlet>
-    </ng-container>
-    <ng-template #elseBlock>
-      <app-hero (conditionChanged)="updateCondition($event)"></app-hero>
-    </ng-template>
-  `,
-  styles: [
-    `
+    selector: 'app-root',
+    standalone: true,
+    template: `
+
+
+
+  <router-outlet></router-outlet> `,
+    styles: [
+        `
       :host {
         max-width: 1280px;
         margin: 0 auto;
@@ -30,25 +23,7 @@ import { HeroService } from './pages/apps/hero/heroservice.compnent';
         text-align: center;
       }
     `,
-  ],
-  imports: [
-    NgIf,
-    HeroComponent,
-    RouterOutlet,
-    SidebarComponent,
-    HeaderComponent,
-    NavbarComponent,
-    AsidebarComponent,
-  ],
+    ],
+    imports: [RouterOutlet, NavbarComponent, AsidebarComponent, HomepageComponent]
 })
-export class AppComponent {
-  condition: boolean;
-
-  constructor(public heroService: HeroService) {
-    this.condition = this.heroService.wanttoenter;
-  }
-
-  updateCondition(newCondition: boolean) {
-    this.condition = newCondition;
-  }
-}
+export class AppComponent {}
--- src/app/pages/apps/hero/heroservice.compnent.ts
+++ src/app/pages/apps/hero/heroservice.compnent.ts
@@ -1,13 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, PLATFORM_ID, Inject } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  wanttoenter: boolean = false;
+  wanttoenter: boolean | undefined;
+
+constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    // Check if the code is running in the browser environment
+    if (isPlatformBrowser(this.platformId)) {
+        // Retrieve the value from localStorage and assert it as a string
+        this.wanttoenter = JSON.parse(localStorage.getItem('wanttoenter') as string) || false;
+        console.log('wanttoenter1', this.wanttoenter);
+    }
+}
 
   enter() {
+    // Toggle the value
     this.wanttoenter = !this.wanttoenter;
-    return this.wanttoenter;
+    // Check if the code is running in the browser environment
+    if (isPlatformBrowser(this.platformId)) {
+      // Store the updated value in localStorage
+      localStorage.setItem('wanttoenter', JSON.stringify(this.wanttoenter));
+      console.log('wanttoenter2', this.wanttoenter);
+    }
   }
 }
