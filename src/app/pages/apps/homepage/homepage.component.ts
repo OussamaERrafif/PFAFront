0fe793103a526317295fd73986f7afe2b20ac1ee
OussamaERrafif <oussamaerra2002@gmail.com>
2024-05-29 18:04:35 +0100

    fixed the homepage and added billings(backend only)

--- src/app/pages/apps/homepage/homepage.component.ts
+++ src/app/pages/apps/homepage/homepage.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [],
   templateUrl: './homepage.component.html',
-  styleUrl: './homepage.component.css'
+  styleUrl: './homepage.component.css',
 })
 export class HomepageComponent {
-
+  //check if the local has a token
+  //if it has a token then go straight to the dashboard
+  //else go to the login page
+  constructor(private router: Router) {}
+  login() {
+    if (localStorage.getItem('token')) {
+      //user route to the dashboard
+      this.router.navigate(['/dashboard']);
+    } else {
+      //user route to the login page
+      this.router.navigate(['/login']);
+    }
+  }
 }
